9d0c5a2c50243d3bc3ac623abe0bfb7ade9ca0a8
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-03-27 19:24:28 +0100

    #2 Dynamic Copyright Year - script done

---
+++ script/date.js
@@ -0,0 +1,2 @@
+let d = new Date();
+document.querySelector("footer p span").innerHTML = d.getFullYear();
--- script/script.js
+++ script/script.js
@@ -3,11 +3,9 @@ const photo = document.getElementById("photo");
 const Pname = document.getElementById("Pname");
 const position = document.getElementById("position");
 
-//function repeatRequest() {
 fetch("./script/testimonials.json")
   .then((response) => response.json())
   .then((data) => {
-    //console.log(typeof data);
 
     let currentIndex = 0;
     function moveCard() {
@@ -17,20 +15,13 @@ fetch("./script/testimonials.json")
       photo.src = testimonial.photo;
       Pname.innerHTML = `<b>${testimonial.name}</b>`;
       position.innerText = testimonial.position;
-      //console.log(element);
     }
-    /* 
-    document.getElementById("left-btn").addEventListener("click",  () => {
-        currentIndex = (currentIndex + 1) % data.length;
-        moveCard();
-      }); */
 
     setInterval(() => {
       currentIndex = (currentIndex + 1) % data.length;
       moveCard();
-    }, 3000);
+    }, 5000);
   })
   .catch((error) => {
     console.error("Error:", error);
   });
-// }
