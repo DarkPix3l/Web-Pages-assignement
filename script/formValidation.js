ba7170e37958034a41d88caa700561bbb7937f14
DarkPix3l <181118522+DarkPix3l@users.noreply.github.com>
2025-03-31 17:16:13 +0200

    JS: fixed typying reset unwanted behaviour - added success validation case with customized allert popup as requested

--- script/formValidation.js
+++ script/formValidation.js
@@ -12,7 +12,7 @@ function validateForm() {
   //accessing error messages divs
   const nameERROR = document.getElementById("nameERROR");
   const emailERROR = document.getElementById("emailERROR");
-  const msgERROR = document.getElementById("msgERROR");
+  const messageERROR = document.getElementById("messageERROR");
 
   //Validation
   const nameRegex = /^[A-Za-z\s]{0,30}$/;
@@ -23,10 +23,9 @@ function validateForm() {
   if (fNAME.value === "") {
     fNAME.style.outline = ERRORborder;
     nameERROR.style.height = heightSize;
-      setTimeout(() => {
-        nameERROR.textContent = "Please fill out all required fields.";
+    setTimeout(() => {
+      nameERROR.textContent = "Please fill out all required fields.";
     }, 200);
-    
   }
   //email
   if (EMAIL.value === "") {
@@ -34,26 +33,24 @@ function validateForm() {
     emailERROR.style.height = heightSize;
     setTimeout(() => {
       emailERROR.textContent = "Please fill out all required fields.";
-  }, 200);
-   
+    }, 200);
   }
   //message
   if (MESSAGE.value === "") {
     MESSAGE.style.outline = ERRORborder;
-    msgERROR.style.height = heightSize;
+    messageERROR.style.height = heightSize;
     setTimeout(() => {
-        msgERROR.textContent = "Please fill out all required fields.";
+      messageERROR.textContent = "Please fill out all required fields.";
     }, 200);
-   
   }
-
 
   //INVALID
   if (!nameRegex.test(fNAME.value)) {
     fNAME.style.outline = ERRORborder;
     nameERROR.style.height = heightSize;
     setTimeout(() => {
-        nameERROR.textContent = "Your Name must be 1-30 letters and separated by a space.";
+      nameERROR.textContent =
+        "Your Name must be 1-30 letters and separated by a space.";
     }, 200);
   }
 
@@ -61,26 +58,41 @@ function validateForm() {
     EMAIL.style.outline = ERRORborder;
     emailERROR.style.height = heightSize;
     setTimeout(() => {
-        emailERROR.textContent = "Invalid email address.";
+      emailERROR.textContent = "Invalid email address.";
     }, 200);
-
   }
+  if (
+    nameRegex.test(fNAME.value.trim()) &&
+    emailRegex.test(EMAIL.value.trim()) &&
+    MESSAGE.value.trim() !== ""
+  ) {
+    Swal.fire({
+      title: "Thank you for reaching out!",
+      text: "Your message has been sent!",
+      icon: "success",
+      background: "#19191a",
+      color: "#FFFF",
+      confirmButtonColor: "#716add",
+    });
 
+    fNAME.value = "";
+    EMAIL.value = "";
+    MESSAGE.value = "";
+  }
 }
 
 //Typing reset
-
 let inputEl = document.querySelectorAll("input, textarea");
-let errorMessages = document.querySelectorAll(".errorMessages");
 
 inputEl.forEach(function (element) {
   element.addEventListener("input", function () {
-
     element.style.outline = "1px solid var(--Primary-Color)";
+    // Get the related error message dynamically
+    let errorMessage = document.getElementById(element.id + "ERROR");
 
-    errorMessages.forEach(function (error) {
-        error.textContent = "";
-        error.style.height = "0px";
-      });
+    if (errorMessage) {
+      errorMessage.textContent = "";
+      errorMessage.style.height = "0px";
+    }
   });
 });
